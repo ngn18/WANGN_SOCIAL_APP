a87c02d7c728651c90f8b0eba59851ea398bc260
Nshimimana Gerald <ngerald18@gmail.com>
2023-07-30 16:02:22 -0700

    Creating CRUD operations and authorizations in the backend

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../contollers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// READ
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/post", verifyToken, getUserPosts);
+
+// UPDATE
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
