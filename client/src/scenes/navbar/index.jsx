3e5ac68bd178726502c02503eaf03da02c5d7f94
Nshimimana Gerald <ngerald18@gmail.com>
2023-08-09 14:26:58 -0700

    Register and login forms

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -19,7 +19,6 @@ import {
     Help,
     Menu,
     Close,
-    Light
 } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
@@ -40,7 +39,7 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    const fullName = `${user.firstName} ${user.lastName}`;
+    const fullName = `${user?.firstName} ${user?.lastName}`;
 
     return <FlexBetween padding="1rem 6%" backroundColor={alt}>
         <FlexBetween gap="1.75rem">
@@ -56,7 +55,7 @@ const Navbar = () => {
                 },
             }}
             >
-
+                WANGN
             </Typography>
             {isNonMobileScreens && (
                 <FlexBetween background={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
@@ -115,6 +114,77 @@ const Navbar = () => {
         )}
 
         {/* MOBILE NAV */}
+        {!isNonMobileScreens && isMobileMenuToggled && (
+            <Box
+                position="fixed"
+                right="0"
+                bottom="0"
+                height="100%"
+                zIndex="10"
+                maxWidth="500px"
+                minWidth="300px"
+                backgroundColor={background}
+            >
+                {/* CLOSE ICON */}
+                <Box display="flex" justifyContent="flex-end" p="1rem">
+                    <IconButton
+                    onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                    >
+                        <Close />
+                    </IconButton>
+                </Box>
+
+                {/* MENU ITEMS */}
+                <FlexBetween
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                gap="3rem"
+                >
+            <IconButton
+              onClick={() => dispatch(setMode())}
+              sx={{ fontSize: "25px" }}
+            >
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: "25px" }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: "25px" }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }} />
+            <Notifications sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogout())}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+
+            </Box>
+        ) }
 
     </FlexBetween>
 
